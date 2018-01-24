50b5da54f4ca6f7c3a4a750d21fab807c6b573ef
sherry <weixiaoli0210@163.com>
2018-01-24 18:13:50 +0800

    完事登录功能

--- application/api/controller/Index.php
+++ application/api/controller/Index.php
@@ -1,11 +1,15 @@
 <?php
 namespace app\api\controller;
+use think\Validate;
+header('Access-Control-Allow-Origin:http://localhost:8080');
+// header("Access-Control-Allow-Methods:GET,POST,PATCH,PUT,OPTIONS");
+header('Access-Control-Allow-Headers: X-Requested-With,X_Requested_With,content-type;charset=utf-8');
+header("Content-type: text/html; charset=utf-8");
 class Index
 {
     /**function：登录 */
     /**author:sherry@2018/1/23 */
     public function login(){
-        
         $username = input('username','');
         $password = input('password','');
         $row=db('user')
@@ -18,7 +22,8 @@ class Index
         }
         
         if(md5($row['userId'].$password)==$row['password']){
-            return json_encode(['code'=>400,'msg'=>'登录成功','data'=>null],JSON_UNESCAPED_UNICODE);
+            db('user')->where('userId',$row['userId'])->setField('loginTime',date('Y-m-d H:i:s'));
+            return json_encode(['code'=>200,'msg'=>'登录成功','data'=>$row],JSON_UNESCAPED_UNICODE);
         }else{
             return json_encode(['code'=>400,'msg'=>'用户名或密码错误','data'=>null],JSON_UNESCAPED_UNICODE);
         }
